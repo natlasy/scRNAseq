f223cb0eb513dbe66c2986b57112807ccd11b2a7
natlasy <35310644+natlasy@users.noreply.github.com>
2018-01-10 22:13:07 +0100

    Update scRNAseq.plate.R

--- QC/scRNAseq.plate.R
+++ QC/scRNAseq.plate.R
@@ -1,2 +1,1 @@
-# This R script helps getting the overall quality information of the cells in a positional approach in sorted plates for single cell RNAseq protocols. The aim is to get an idea on the bias that may happen in the cell sorting or processing steps of the protocol across several plates.
-# The input is a data matrix contains features in the rows and the single cells in the columns.
+# To run the script, put the count table and the index files in the same directory as the script is and in linux just type: Rscript scRNAseq.plate.R
